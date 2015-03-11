27a3c266fe9bea374644768cebac4d7674a53c3f
scottteran <wayteran@gmail.com>
2015-03-11 16:55:37 -0600

    added js file and jquery

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+});
+
+$('ul').on('click', 'li', function(event){
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});
+
+// notes: use .val(), .prepend(), .append() 
