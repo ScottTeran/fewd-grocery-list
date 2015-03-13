b5de470964f3598c1283105aadd43ea3780266ea
scottteran <wayteran@gmail.com>
2015-03-13 09:42:02 -0600

    append() working in js file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
-// click button, add to list
+	// click button, add to list
 
-$("#btn1").click(function(){
-	$("#need-list").append("<li></li>");
-})
+	$("#btn1").click(function(){
+		$("#need-list").append($("<li>", { text: $("#need-input").val()
+		}));
+	});
 
-
-});
-
-$('ul').on('click', 'li', function(event){
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
-});
+	$('ul').on('click', 'li', function(event){
+	  // fires when any LIs are clicked on
+	  $("li").hide();
+	  // including LIs that aren't on the page when it is initially loaded
+	});
 
 // notes: use .val(), .prepend(), .append()	 
+
+});
+
